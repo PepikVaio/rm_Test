25025485f46b32a3d801ded9e48480f362404855
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2026-07-09 21:42:46 +0200

    Implement file read and write in diacritics.py
    Add functionality to read from README.md and write to README_result.md

--- tools/diacritics.py
+++ tools/diacritics.py
@@ -1,1 +1,11 @@
-asx
+from pathlib import Path
+
+input_file = Path("README.md")
+output_file = Path("README_result.md")
+
+text = input_file.read_text(encoding="utf-8")
+
+# zatím jen test průchodu
+output_file.write_text(text, encoding="utf-8")
+
+print("Hotovo")
